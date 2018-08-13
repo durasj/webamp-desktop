bea4028320737d3bc6e2a6501a38c4e8adf6416e
Jakub Ďuraš <jakub@duras.me>
2018-08-13 19:36:44 +0200

    Rebrand to webamp, bump dependencies, improve security
    + neccessary changes to work with the new webamp
    + cleanup in dependencies etc.
    + codestyle changes
    + monkey patching removed!

---
+++ preload.js
@@ -0,0 +1,24 @@
+const remote = require('electron').remote
+
+// We want to completely disable the eval() for security reasons
+// ESLint will warn about any use of eval(), even this one
+// eslint-disable-next-line
+window.eval = global.eval = function () {
+    throw new Error(`Sorry, this app does not support window.eval().`)
+}
+
+/**
+ * Electron API wrappers passed to the renderer
+ *
+ * Only implementation of the selected APIs is available
+ * to greatly reduce the attack surface
+ * See https://github.com/electron/electron/blob/master/docs/tutorial/security.md#2-disable-nodejs-integration-for-remote-content
+ */
+
+window.minimizeElectronWindow = function () {
+    return remote.getCurrentWindow().minimize()
+}
+
+window.closeElectronWindow = function () {
+    return remote.getCurrentWindow().close()
+}
